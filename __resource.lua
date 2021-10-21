427b6041cee691b0d5bb6c1e99ed4e4d5d707368
InteLCore-star <67869025+InteLCore-star@users.noreply.github.com>
2021-10-21 19:51:14 +0800

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,9 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
 
+game 'gta5'
 
-client_scripts {
-    'client.lua'
+description "NakreS UI Obje Spawner  Discord : NakreS#8888"
+
+client_script {
+    client.lua
 }
